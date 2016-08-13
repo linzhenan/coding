f7701a94334d26c3b164e1eb78be118b5aae517e
Zhenan Lin <zhenanlin@outlook.com>
2016-08-13 22:44:14 +0800

    GoogleAPECTest-Practice Problem A/B

--- OnlineContest/GoogleAPECTest-PracticeRound/main1.cpp
+++ OnlineContest/GoogleAPECTest-PracticeRound/main1.cpp
@@ -6,33 +6,61 @@
 #pragma warning(disable:4996)
 #endif
 
+int clip(int val, int minVal, int maxVal)
+{
+	if (val < minVal)
+		return minVal;
+	else if (val > maxVal)
+		return maxVal;
+	else
+		return val;
+}
+int dist(char *str, int i, int j) {
+	int len = strlen(str);
+	i = clip(i, 0, len - 1);
+	j = clip(j, 0, len - 1);
+	if (j < i || j - i > 2)
+		return -1;
+
+	if (j - i == 0)
+		return 1;
+	else if (j - i == 1)
+		return str[i] == str[j] ? 1 : 2;
+	else if (j - i == 2)
+	{
+		if (str[i] == str[i + 1] && str[i + 1] == str[i + 2])
+			return 1;
+		else if (str[i] == str[i + 1] && str[i + 1] != str[i + 2])
+			return 2;
+		else if (str[i] != str[i + 1] && str[i + 1] == str[i + 2])
+			return 2;
+		else if (str[i] != str[i + 1] && str[i] == str[i + 2])
+			return 2;
+		else
+			return 3;
+	}
+	else
+		return -1;
+}
 int main() {
 	int t;
 	char str[1001];
 	int len;
 	long long ans = 1;
 	int factor = 0;
-	double ansd = 0.0;
 
 	scanf("%d", &t);
 	for (int i = 1; i <= t; i++) {
 		scanf("%s", str);
 		len = strlen(str);
 		ans = 1;
-		ansd = 1.0;
-		for (int j = 0; j < len; j++) {
-			factor = 1;
-			if (j > 0       && str[j - 1] != str[j])
-				factor++;
-			if (j < len - 1 && str[j + 1] != str[j])
-				factor++;
-			if (j > 0 && j < len - 1 && str[j - 1] == str[j + 1] && str[j - 1] != str[j])
-				factor--;
+		for (int j = 0; j < len; j++)
+		{
+			factor = dist(str, j - 1, j + 1);
 			ans *= factor;
-			ansd *= factor;
+			ans %= 1000000007;
 		}
-		printf("Case #%d: %" PRIu64 "\n", i, ans % 1000000007);
-		printf("Case #%d: %lf\n", i, ansd);
+		printf("Case #%d: %" PRIu64 "\n", i, ans);
 	}
 	return 0;
 }
--- OnlineContest/GoogleAPECTest-PracticeRound/main2.cpp
+++ OnlineContest/GoogleAPECTest-PracticeRound/main2.cpp
@@ -1,12 +1,16 @@
 #include <cstdio>
 #include <cstring>
 #include <cinttypes>
+#include <unordered_map>
+
+using namespace std;
 
 #ifdef _MSC_VER
 #pragma warning(disable:4996)
 #endif
 
-int main() {
+void smallcase()
+{
 	int t;
 	int n, k;
 	int num[4][1000];
@@ -22,16 +26,70 @@ int main() {
 			}
 		}
 		for (int l0 = 0; l0 < n; l0++)
+		{
 			for (int l1 = 0; l1 < n; l1++)
+			{
 				for (int l2 = 0; l2 < n; l2++)
-					for (int l3 = 0; l3 < n; l3++) {
-						if ((num[0][l0] ^ num[1][l1] ^ num[2][l2] ^ num[3][l3]) == k) {
-							//printf("%d %d %d %d\n", num[0][l0], num[1][l1], num[2][l2], num[3][l3]);
+				{
+					for (int l3 = 0; l3 < n; l3++)
+					{
+						if ((num[0][l0] ^ num[1][l1] ^ num[2][l2] ^ num[3][l3]) == k)
+						{
 							ans++;
 						}
 					}
+				}
+			}
+		}
+
 		printf("Case #%d: %d\n", i, ans);
 	}
+}
 
+void largecase()
+{
+	int T, t;
+	int n;
+	long long k;
+	long long num[4][1000];
+	long long ans;
+	unordered_map<long long, long long> map1, map2;
+
+	scanf("%d", &T);
+	for (int t = 1; t <= T; t++)
+	{
+		ans = 0;
+		map1.clear();
+		map2.clear();
+
+		scanf("%d%" PRIu64, &n, &k);
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < n; j++) {
+				scanf("%" PRIu64, &num[i][j]);
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				map1[num[0][i] ^ num[1][j]]++;
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				map2[num[2][i] ^ num[3][j]]++;
+
+		for (auto e : map1)
+		{
+			if (map2.find(e.first ^ k) != map2.end())
+			{
+				ans += e.second * map2[e.first ^ k];
+			}
+		}
+
+		printf("Case #%d: %" PRIu64 "\n", t, ans);
+	}
+}
+
+int main() {
+	largecase();
 	return 0;
 }
